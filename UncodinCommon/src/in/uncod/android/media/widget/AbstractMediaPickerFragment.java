b4d3279d654028bc7932a24c1f5d798e1b25d4a4
Cameron Currie <me@cameroncurrie.net>
2012-12-03 14:56:18 -0600

    Clean up code (unused imports, etc.)

--- UncodinCommon/src/in/uncod/android/media/widget/AbstractMediaPickerFragment.java
+++ UncodinCommon/src/in/uncod/android/media/widget/AbstractMediaPickerFragment.java
@@ -7,11 +7,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * Created by IntelliJ IDEA. User: ddrboxman Date: 4/4/12 Time: 12:26 PM
