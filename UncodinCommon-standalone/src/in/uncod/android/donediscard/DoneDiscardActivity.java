85a674f5d65d0af8f0015678b883951ba64cc311
Colin Edwards <colin@recursivepenguin.com>
2013-12-04 14:22:08 -0600

    Stuff for blurring

--- UncodinCommon-standalone/src/in/uncod/android/donediscard/DoneDiscardActivity.java
+++ UncodinCommon-standalone/src/in/uncod/android/donediscard/DoneDiscardActivity.java
@@ -23,6 +23,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import in.uncod.android.R;
+
 public abstract class DoneDiscardActivity extends ActionBarActivity {
 
     public abstract void doneClick();
